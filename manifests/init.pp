0310826fbbb317bdcb69a645fab734a24eb4273e
Oleh Samorai <oleg.samorai@gmail.com>
2015-06-02 12:22:39 +0300

    test again

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,4 @@
+hiera_include('classes')
 file { 'sshd_config':
   path    => '/etc/ssh/sshd_config',
   ensure  => file,
@@ -49,4 +50,51 @@ service { 'sshd':
   name      => 'ssh',
   subscribe => File['sshd_config'],
   require   => Package['sshd']
+}
+
+file { '/etc/puppet/hiera.yaml':
+  ensure => present,
+  content => '
+  ---
+  :hierarchy:
+    - "nodes/%{::fqdn}"
+    - "manufacturers/%{::manufacturer}"
+    - "virtual/%{::virtual}"
+    - common
+  :backends:
+    - yaml
+  :yaml:
+    :datadir: "/etc/puppet/environments/%{::environment}/hieradata"
+',
+  mode => 0644,
+  owner => root,
+  group => puppet
+}
+
+file { "/var/cache/r10k":
+  ensure => "directory",
+  mode => 2775,
+  owner => root,
+  group => puppet
+}
+
+file { '/etc/r10k.yaml':
+  ensure => present,
+  content => "
+  # location for cached repos
+:cachedir: '/var/cache/r10k'
+
+# git repositories containing environments
+:sources:
+  :base:
+    remote: '/srv/puppet.git'
+    basedir: '/etc/puppet/environments'
+
+# purge non-existing environments found here
+:purgedirs:
+  - '/etc/puppet/environments'
+  ",
+  mode => 0644,
+  owner => root,
+  group => puppet
 }
