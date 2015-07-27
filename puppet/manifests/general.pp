5ea0610681c9bdabb5a4207dbf983eff933f8aff
Oleh Samorai <oleg.samorai@gmail.com>
2015-07-27 19:14:39 +0300

    mbank web mobile

--- puppet/manifests/general.pp
+++ puppet/manifests/general.pp
@@ -1,6 +1,5 @@
-class sethostname {
-
-  $host_name = "serega.wallet.best"
+node default {
+  $host_name = "sandbox-mobile.wallet.best"
   file { "/etc/hostname":
     ensure => present,
     owner => root,
@@ -12,39 +11,5 @@ class sethostname {
   exec { "set-hostname":
     command => "/bin/hostname -F /etc/hostname",
     unless => "/usr/bin/test `hostname` = `/bin/cat /etc/hostname`",
-  }
-}
-
-node default {
-
-  include sethostname
-
-  class { 'nginx':
-    daemon_user => 'www-data',
-    worker_processes => 4,
-    pid => '/run/nginx.pid',
-    worker_connections => 1024,
-    multi_accept => 'on',
-    events_use => 'epoll',
-    sendfile => 'on',
-    http_tcp_nopush => 'on',
-    http_tcp_nodelay => 'on',
-    keepalive_timeout => '65',
-    types_hash_max_size => '2048',
-    server_tokens => 'off',
-  }
-
-  $nginx = "nginx.conf"
-
-  file { "/etc/nginx/sites-enabled/mbank.api.serega.conf":
-    ensure => link,
-    target => "/www/mbank.api.serega/$nginx",
-    notify => Service["nginx"],
-  }
-
-  file { "/etc/nginx/sites-enabled/autodeployer.conf":
-    ensure => link,
-    target => "/www/nebo15.rome/www/config/nginx.conf",
-    notify => Service["nginx"],
   }
 }
--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -1,12 +1,3 @@
-define sudo-include($name, $content) {
-  file { "/etc/sudoers.d/$name":
-    content => $content,
-    mode => 0440,
-    owner => root,
-    group => root,
-  }
-}
-
 node default {
 
   package {'install uuid-runtime':
@@ -15,28 +6,28 @@ node default {
   }
 
   include stdlib
-  include composer
   include apt
 
   class{'mbank_api_users':} ->
-  class {'mbank_api_php56':} -> class{'mbank_api_mongo':} ->
 
 
-  file { ["/www", "/var/backups/mbank.api", "/var/www", "/var/www/.ssh", "/var/log", "/var/log/www"]:
+  file { ["/www", "/var/www", "/var/www/.ssh", "/var/log", "/var/log/www"]:
     ensure => "directory",
     owner  => "www-data",
     group  => "www-data",
     mode   => 755
   }
 
-  sudo-include { 'init_www_data_user':
-    name =>  "www-data-user",
+  file { "/etc/sudoers.d/www-data-user":
     content => "\
 Cmnd_Alias        CMDS = /usr/bin/puppet
 www-data  ALL=NOPASSWD: CMDS
-"
+",
+    mode => 0440,
+    owner => root,
+    group => root,
   }
-  
+
   package { "openssh-server": ensure => "installed" }
 
   service { "ssh":
