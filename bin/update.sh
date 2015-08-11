ab09b9db225b7260fa7a7a31d258e92e6b8560ca
Oleh Samorai <oleg.samorai@gmail.com>
2015-08-11 13:48:50 +0300

    fix updater

--- bin/update.sh
+++ bin/update.sh
@@ -2,6 +2,11 @@
 PROJECT_DIR=$(dirname $0)/../
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 git pull origin $BRANCH
-sudo puppet apply --modulepath /www/nebo15.rome/puppet/modules /www/nebo15.rome/puppet/manifests/init.pp
-sudo puppet apply --modulepath /www/nebo15.rome/puppet/modules /www/nebo15.rome/puppet/manifests/general.pp
+if [ "$BRANCH" = "master" ]; then
+    enviroment='prod'
+else
+    enviroment='dev'
+fi
+sudo FACTER_server_tags="role:${enviroment}" puppet apply --modulepath /www/nebo15.rome/puppet/modules /www/nebo15.rome/puppet/manifests/init.pp
+sudo FACTER_server_tags="role:${enviroment}" puppet apply --modulepath /www/nebo15.rome/puppet/modules /www/nebo15.rome/puppet/manifests/general.pp
 
