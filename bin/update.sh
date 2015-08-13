55a7704c540edc18cb09a2e547757a12c90ce4ec
Oleh Samorai <oleg.samorai@gmail.com>
2015-08-13 14:43:46 +0300

    autodeployer

--- bin/update.sh
+++ bin/update.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 PROJECT_DIR=$(dirname $0)/../
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
-git pull origin $BRANCH
-if [ "$BRANCH" = "master" ]; then
-    enviroment='prod'
-else
-    enviroment='dev'
+while getopts "e:" OPTION
+do
+     case ${OPTION} in
+         e)
+             enviroment=$OPTARG
+             ;;
+     esac
+done
+
+if [${enviroment} == '']
+then
+echo 'You should use -e environment [dev|prod]';
+exit;
 fi
+
+git pull origin $BRANCH
 sudo FACTER_server_tags="role:${enviroment}" puppet apply --modulepath /www/nebo15.rome/puppet/modules /www/nebo15.rome/puppet/manifests/init.pp
 sudo FACTER_server_tags="role:${enviroment}" puppet apply --modulepath /www/nebo15.rome/puppet/modules /www/nebo15.rome/puppet/manifests/general.pp
 
