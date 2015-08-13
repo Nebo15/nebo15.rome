9575d97cd3f6df95cbe3b136821ca4b5d46770bb
Oleh Samorai <oleg.samorai@gmail.com>
2015-08-13 19:11:20 +0300

    autodeployer

--- www/index.php
+++ www/index.php
@@ -2,4 +2,13 @@
 error_reporting('E_ALL');
 ini_set('display_errors', 'On');
 $postdata = file_get_contents("php://input");
-file_put_contents(dirname(__FILE__) . '/logs/main.log', $postdata);
+if (is_string($postdata)) {
+    file_put_contents(dirname(__FILE__) . '/logs/main.log', $postdata . PHP_EOL, FILE_APPEND);
+    $data = json_decode($postdata);
+    $project = $data->project;
+    $commit = $data->commit;
+    $file = '/www/' . $project . '/bin/update.sh';
+    if (file_exists($file)) {
+        exec('/bin/bash ' . $file . ' -c ' . $commit, $a);
+    }
+}
