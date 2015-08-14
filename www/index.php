512e6124f4f1ecddbed23e44b13668c941ff5f8f
Oleh Samorai <oleg.samorai@gmail.com>
2015-08-14 15:23:28 +0300

    autodeploy on rome

--- www/index.php
+++ www/index.php
@@ -6,10 +6,8 @@ if (is_string($postdata)) {
     file_put_contents(dirname(__FILE__) . '/logs/main.log', $postdata . PHP_EOL, FILE_APPEND);
     $data = json_decode($postdata);
     $project = $data->project;
-    $commit = $data->commit;
     $file = '/www/' . $project . '/bin/update.sh';
     if (file_exists($file)) {
-        exec('/bin/bash ' . $file . ' -c ' . $commit, $a);
+        exec('/bin/bash ' . $file , $a);
     }
 }
-//test_info
