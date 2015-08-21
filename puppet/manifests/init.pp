5b690a6a184411868408a72987b941f21bf45684
Oleh Samorai <oleg.samorai@gmail.com>
2015-08-21 18:54:20 +0300

    enable autoupdate mbank web

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -7,7 +7,7 @@ node default {
 
   $new_relic_licence_key = "fc04150b6b2478740bd6a6357087c1342bf99789"
   $new_relic_app_name = 'wallet.best.web.production'
-  class {'mbank_api_php56':} ->
+  class{'enable_autoupdate':} -> class {'mbank_api_php56':} ->
   class {'newrelic::server::linux':
     newrelic_license_key  => $new_relic_licence_key,
   } ~>
