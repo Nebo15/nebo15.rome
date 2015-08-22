0de557ce2836b0e66260679c700c5ab171c5ae8e
Oleh Samorai <oleg.samorai@gmail.com>
2015-08-22 17:08:25 +0300

    update deployer mobile

--- www/index.php
+++ www/index.php
@@ -1,5 +1,14 @@
 <?php
 error_reporting('E_ALL');
 ini_set('display_errors', 'On');
-exec('/bin/bash /www/nebo15.rome/bin/update.sh', $a);
-print_r($a);
+$postdata = file_get_contents("php://input");
+if (is_string($postdata)) {
+    file_put_contents(dirname(__FILE__) . '/logs/main.log', $postdata . PHP_EOL, FILE_APPEND);
+    $data = json_decode($postdata);
+    $project = $data->project;
+    $branch = $data->branch;
+    $file = '/www/' . $project . '/bin/update.sh';
+    if (file_exists($file)) {
+        exec('/bin/bash ' . $file  . ' -b ' . $branch, $a);
+    }
+}
